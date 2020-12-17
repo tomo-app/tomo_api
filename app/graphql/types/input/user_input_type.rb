66001a990330cc8eb807d142e316dacd55db394d
Logan Riffell <lkriffell@gmail.com>
2020-12-17 15:31:43 -0700

    Add password and id as a UserInputType they are not required unless for specific queries/mutations

--- app/graphql/types/input/user_input_type.rb
+++ app/graphql/types/input/user_input_type.rb
@@ -4,7 +4,8 @@ module Types
         argument :email, String, required: true
         argument :username, String, required: true
         argument :password, String, required: true
-        argument :password_confirmation, String, required: true
+        argument :password_confirmation, String, required: false
+        argument :id, String, required: false
       end
     end
   end
