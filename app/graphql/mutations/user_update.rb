2a0d4d1c1df05d8898d0d5751ed6541d29e674f6
Logan Riffell <lkriffell@gmail.com>
2020-12-17 15:37:25 -0700

    Add userUpdate spec and add sad path to userUpdate when no id is provided

--- app/graphql/mutations/user_update.rb
+++ app/graphql/mutations/user_update.rb
@@ -5,14 +5,18 @@ module Mutations
   
       def resolve(params:)
         user_params = Hash params
-        begin
-        user = User.find(user_params[:id])
-        user.update!(user_params)
+        if user_params[:id] != nil
+            begin
+            user = User.find(user_params[:id])
+            user.update!(user_params)
 
-        { user: user }
-        rescue ActiveRecord::RecordInvalid => e
-        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
-            " #{e.record.errors.full_messages.join(', ')}")
+            { user: user }
+            rescue ActiveRecord::RecordInvalid => e
+            GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+                " #{e.record.errors.full_messages.join(', ')}")
+            end
+        else
+            GraphQL::ExecutionError.new("You must provide a user id to update a user")
         end
       end
     end
