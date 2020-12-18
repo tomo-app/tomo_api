10c369c709adaf5833d2029a2a459fde6e8f58bc
Logan Riffell <lkriffell@gmail.com>
2020-12-18 14:17:50 -0700

    DRY up tests

--- spec/mutations/users/user_sign_up_spec.rb
+++ spec/mutations/users/user_sign_up_spec.rb
@@ -47,6 +47,12 @@ module Mutations
                     expect(bob["errors"][0]["message"]). to eq("passwords must match")
                 end
 
+                it "A user cannot be created with an unknown attribute" do 
+                    bob = TomoApiSchema.execute(@user_sign_up, variables: { input: { params: { email: "JB@email.com", name: "Jim Bobby", password: "1234", passwordConfirmation: "1234" } }})
+
+                    expect(bob["errors"][0]["message"]). to eq("Variable $input of type UserSignUpInput! was provided invalid value for params.name (Field is not defined on UserInput)")
+                end
+
                 it "A user cannot be created with a missing passwordConfirmation" do 
                     bob = TomoApiSchema.execute(@user_sign_up, variables: { input: { params: { email: "JB@email.com", username: "Jim Bobby", password: "1234" } }})
 
--- spec/mutations/users/user_update_spec.rb
+++ spec/mutations/users/user_update_spec.rb
@@ -4,6 +4,7 @@ module Mutations
     module Users
         RSpec.describe UserUpdate, type: :request do
             before :each do
+                @old_user = User.create(email: "JB@email.com", username: "Jim Bobby", password: "1234")
                 @user_update = <<-GRAPHQL
                                 mutation($input: UserUpdateInput!) {
                                     userUpdate(input: $input) {
@@ -18,8 +19,7 @@ module Mutations
             end
             describe "Happy Paths - " do
                 it "A user can be updated" do
-                    old_user = User.create(email: "JB@email.com", username: "Jim Bobby", password: "1234")
-                    updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { id: old_user.id.to_s, email: "updated_value@email.com", username: "updated_value", password:"1234" } }})
+                    updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { id: @old_user.id.to_s, email: "updated_value@email.com", username: "updated_value", password:"1234" } }})
 
                     expect(updated_user["data"]["userUpdate"]["user"]["id"]).to_not eq(nil)
 
@@ -28,20 +28,23 @@ module Mutations
                 end
 
                 it "A user can update their password" do
-                    old_user = User.create(email: "JB@email.com", username: "Jim Bobby", password: "1234")
-                    updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { id: old_user.id.to_s, email: "JB@email.com", username: "Jim Bobby", password:"new_password" } }})
-                    updated_user = User.find(old_user.id)
-                    
-                    expect(old_user.password).to_not eq(updated_user.password)
+                    updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { id: @old_user.id.to_s, email: "JB@email.com", username: "Jim Bobby", password:"new_password" } }})
+                    updated_user = User.find(@old_user.id)
+
+                    expect(@old_user.password).to_not eq(updated_user.password)
                 end
             end
 
             describe "Sad Paths - " do
                 it "A user cannot be updated without providing id" do
-                    user = User.create(email: "JB@email.com", username: "Jim Bobby", password: "1234")
                     non_updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { email: "updated_value@email.com", username: "updated_value", password:"1234" } }})
 
                     expect(non_updated_user["errors"][0]["message"]).to eq("You must provide a user id to update a user")
+                end
+                it "A user cannot be updated when provided invalid attributes" do
+                    non_updated_user = TomoApiSchema.execute(@user_update, variables: { input: { params: { id: @old_user.id.to_s, email: "updated_value@email.com", name: "updated_value", password:"1234" } }})
+
+                    expect(non_updated_user["errors"][0]["message"]).to eq("Variable $input of type UserUpdateInput! was provided invalid value for params.name (Field is not defined on UserInput)")
                 end
             end
         end
