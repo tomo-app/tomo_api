0a34c099bfddb0e5e70e3bc8d5a108e3c3bce02b
Logan Riffell <lkriffell@gmail.com>
2020-12-18 14:18:44 -0700

    Add types for availability

--- app/graphql/types/input/user_input_type.rb
+++ app/graphql/types/input/user_input_type.rb
@@ -1,11 +1,17 @@
 module Types
     module Input
       class UserInputType < Types::BaseInputObject
-        argument :email, String, required: true
-        argument :username, String, required: true
-        argument :password, String, required: true
+        # Users
+        argument :email, String, required: false
+        argument :username, String, required: false
+        argument :password, String, required: false
         argument :password_confirmation, String, required: false
         argument :id, String, required: false
+        # Availability
+        argument :user_id, String, required: false
+        argument :start_date_time, String, required: false
+        argument :end_date_time, String, required: false
+        argument :status, Integer, required: false
       end
     end
   end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :user_sign_up, mutation: Mutations::UserSignUp
     field :user_update, mutation: Mutations::UserUpdate
+    field :create_availability, mutation: Mutations::CreateAvailability
   end
 end
