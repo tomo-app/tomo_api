382aa2608af2260661e88a293eb2e28de1f5307d
Logan Riffell <lkriffell@gmail.com>
2020-12-18 14:18:17 -0700

    Add createAvailability mutation

---
+++ app/graphql/mutations/create_availability.rb
@@ -0,0 +1,20 @@
+module Mutations
+    class CreateAvailability < Mutations::BaseMutation
+        argument :params, Types::Input::UserInputType, required: true
+    
+        field :availability, Types::AvailabilityType, null: false
+    
+        def resolve(params:)
+          availability_params = Hash params
+
+            begin
+              availability = Availability.create!(availability_params)
+      
+              { availability: availability }
+            rescue ActiveRecord::RecordInvalid => e
+              GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+                " #{e.record.errors.full_messages.join(', ')}")
+            end
+        end
+    end
+end
