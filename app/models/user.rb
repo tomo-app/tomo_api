0cf93d021a64ba465d69344df045551a0a21ac12
Leah Riffell <leah.riffell@gmail.com>
2020-12-17 20:09:52 -0700

    Update the alias of user on blocked_pairings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :pairings
-  has_many :blocked_pairings
+  has_many :blocked_pairings, foreign_key: 'blocking_user_id', class_name: 'BlockedPairing'
   has_many :user_languages
   has_many :languages, through: :user_languages
 
