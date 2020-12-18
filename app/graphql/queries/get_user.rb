b3106b49cef8082d738db291c5bb3286f40fdc1d
Logan Riffell <lkriffell@gmail.com>
2020-12-17 20:01:05 -0700

    Add an empty line

--- app/graphql/queries/get_user.rb
+++ app/graphql/queries/get_user.rb
@@ -2,7 +2,7 @@ module Queries
     class GetUser < Queries::BaseQuery
       type Types::UserType, null: false
       argument :id, ID, required: true
-  
+      
       def resolve(id:)
         User.find(id)
       rescue ActiveRecord::RecordNotFound => _e
