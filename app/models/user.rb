0c82c6d4d97471d9545051a3e124a0408b6fe346
Leah Riffell <leah.riffell@gmail.com>
2020-12-29 16:15:30 -0700

    Add more thorough testing to get user availabilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :pairings, dependent: :destroy
   has_many :pairings, foreign_key: 'user1_id', class_name: 'Pairing', dependent: :destroy, inverse_of: :user1
   has_many :pairings, foreign_key: 'user2_id', class_name: 'Pairing', dependent: :destroy, inverse_of: :user2
-  has_many :blocked_pairings, foreign_key: 'blocking_user_id', class_name: 'BlockedPairing', dependent: :destroy, inverse_of: :blocking_user
+  has_many :blocked_pairings, foreign_key: 'blocking_user_id', class_name: 'BlockedPairing', dependent: :destroy,
+                              inverse_of: :blocking_user
   has_many :user_languages, dependent: :destroy
   has_many :languages, through: :user_languages
   has_many :availabilities, dependent: :destroy
