3b2ac00c340f9eebfd24258cf9c051acf9ece56d
Leah Riffell <leah.riffell@gmail.com>
2020-12-29 16:18:03 -0700

    Remove created and updated dates from availability factory

--- spec/factories/availabilities.rb
+++ spec/factories/availabilities.rb
@@ -2,8 +2,6 @@ FactoryBot.define do
   factory :availability do
     start_date_time { Faker::Time.between(from: DateTime.new(2021, 1, 1), to: DateTime.new(2022, 12, 31)).to_i }
     end_date_time { Faker::Time.between(from: DateTime.new(2021, 1, 1), to: DateTime.new(2022, 12, 31)).to_i }
-    created_at { Faker::Time.between(from: DateTime.new(2021, 1, 1), to: DateTime.new(2022, 12, 31)) }
-    updated_at { Faker::Time.between(from: DateTime.new(2021, 1, 1), to: DateTime.new(2022, 12, 31)) }
     association :user
   end
 end
