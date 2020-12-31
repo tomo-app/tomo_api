4dfdf009343ab8b801aa4d194bd5efd76316986c
Logan Riffell <lkriffell@gmail.com>
2020-12-31 12:17:56 -0700

    Comply with rubocops demands

--- app/graphql/mutations/user_languages/create_user_language.rb
+++ app/graphql/mutations/user_languages/create_user_language.rb
@@ -7,9 +7,7 @@ module Mutations
       def resolve(params:)
         user_language_params = Hash params
         user_language_params[:fluency_level] = user_language_params[:fluency_level].to_i
-        if User.find(user_language_params[:user_id]) && Language.find(user_language_params[:language_id])
-          UserLanguage.create(user_language_params)
-        end
+        UserLanguage.create(user_language_params)
       end
     end
   end
