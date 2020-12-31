4ea212cb5b0e8d99fc55cc0c5d9985f005cda6da
Logan Riffell <lkriffell@gmail.com>
2020-12-31 12:02:29 -0700

    Add language factory

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
   factory :language do
     name { Faker::ProgrammingLanguage.name }
-
-    trait :with_user_languages do
-      after(:create) do |language|
-        create(:userlanguage, language: language)
-      end
-    end
   end
 end
