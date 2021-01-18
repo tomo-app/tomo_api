8c417191f4b0a3d3d4493cfa715b276de7027d72
Logan Riffell <lkriffell@gmail.com>
2021-01-18 13:43:48 -0700

    Add query to query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
-    field :get_users, [Types::UserType], null: false, description: 'Returns a list of users'
+    field :get_languages, [Types::LanguageType], null: false, description: 'Availabilities (sorted newest to oldest) by user_id and status'
 
-    field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
-      argument :id, ID, required: true
+    def get_languages
+      Language.all
     end
-
+    
     field :get_availabilities, [Types::AvailabilityType], null: false, description: 'Availabilities (sorted newest to oldest) by user_id and status' do
       argument :user_id, ID, required: true
       argument :status, String, required: true
@@ -17,9 +17,15 @@ module Types
         .order(start_date_time: :asc)
     end
 
+    field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
+      argument :id, ID, required: true
+    end
+
     def get_users
       User.all
     end
+
+    field :get_users, [Types::UserType], null: false, description: 'Returns a list of users'
 
     def get_user(id:)
       User.find(id)
