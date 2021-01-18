cad96b44530593fffe17f8422bf9a86f6e7cbd92
Logan Riffell <lkriffell@gmail.com>
2021-01-18 15:09:39 -0700

    Add user_languages to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,6 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :availabilities, [Types::AvailabilityType], null: false
+    field :user_languages, [Types::UserLanguageType], null: false
   end
 end
