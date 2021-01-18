2c6fba5f425c4590ed6ee7b6f166a5f546fd85de
Logan Riffell <lkriffell@gmail.com>
2021-01-18 15:11:17 -0700

    Add a test that updateUser can create target and native userLanguages

--- spec/mutations/users/update_user_spec.rb
+++ spec/mutations/users/update_user_spec.rb
@@ -5,6 +5,8 @@ module Mutations
     RSpec.describe UpdateUser, type: :request do
       before :each do
         @existing_user = User.create!(email: 'JB@email.com', username: 'Jim', password: '1234')
+        @target = create :language
+        @native = create :language
       end
 
       it "A user can be updated" do
@@ -29,6 +31,41 @@ module Mutations
         expect(json['data']['updateUser']['id']).to_not eq(nil)
         expect(json['data']['updateUser']['email']).to eq('newme@email.com')
         expect(json['data']['updateUser']['username']).to eq('newme')
+      end
+
+      it "A user can add a target and native language" do
+        query = <<~GQL
+          mutation {
+            updateUser(input: {
+              id: #{@existing_user.id}
+              targetId: "#{@target.id}"
+              nativeId: "#{@native.id}"
+            }) {
+              id
+              userLanguages{
+                id
+                userId
+                languageId
+                fluencyLevel
+              }
+            }
+          }
+        GQL
+
+        post '/graphql', params: { query: query }
+
+        json = JSON.parse(response.body)
+
+        expect(json['data']['updateUser']['id']).to_not eq(nil)
+        expect(json['data']['updateUser']['userLanguages'][0]['id']).to_not eq(nil)
+        expect(json['data']['updateUser']['userLanguages'][0]['userId']).to eq(@existing_user.id.to_s)
+        expect(json['data']['updateUser']['userLanguages'][0]['languageId']).to eq(@target.id.to_s)
+        expect(json['data']['updateUser']['userLanguages'][0]['fluencyLevel']).to eq("target")
+
+        expect(json['data']['updateUser']['userLanguages'][1]['id']).to_not eq(nil)
+        expect(json['data']['updateUser']['userLanguages'][1]['userId']).to eq(@existing_user.id.to_s)
+        expect(json['data']['updateUser']['userLanguages'][1]['languageId']).to eq(@native.id.to_s)
+        expect(json['data']['updateUser']['userLanguages'][1]['fluencyLevel']).to eq("native")
       end
     end
   end
