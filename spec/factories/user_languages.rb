d4d6ff0973c1414b9c0e443b5d5ccfb9a4fb2f93
Logan Riffell <lkriffell@gmail.com>
2021-01-18 15:09:24 -0700

    Add user_languages factory

---
+++ spec/factories/user_languages.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user_language do
+    trait :native do
+      fluency_level { 0 }
+    end
+    
+    trait :target do
+      fluency_level { 1 }
+    end
+  end
+end
