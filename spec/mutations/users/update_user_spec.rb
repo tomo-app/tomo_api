3c2443618cecb6c2fef1499947c47dc2410d57da
Logan Riffell <lkriffell@gmail.com>
2021-01-18 16:38:26 -0700

    Add checks if a user has any userLanguages to update

--- spec/mutations/users/update_user_spec.rb
+++ spec/mutations/users/update_user_spec.rb
@@ -5,69 +5,100 @@ module Mutations
     RSpec.describe UpdateUser, type: :request do
       before :each do
         @existing_user = User.create!(email: 'JB@email.com', username: 'Jim', password: '1234')
+        @user_with_no_languages = create :user
         @target = create :language
         @native = create :language
         create(:user_language, :target, user: @existing_user, language: @target)
         create(:user_language, :native, user: @existing_user, language: @native)
       end
 
-      it "A user can be updated" do
-        query = <<~GQL
-          mutation {
-            updateUser(input: {
-              id: #{@existing_user.id}
-              email: "newme@email.com"
-              username: "newme"
-            }) {
-              id
-              username
-              email
-            }
-          }
-        GQL
-
-        post '/graphql', params: { query: query }
-
-        json = JSON.parse(response.body)
-
-        expect(json['data']['updateUser']['id']).to_not eq(nil)
-        expect(json['data']['updateUser']['email']).to eq('newme@email.com')
-        expect(json['data']['updateUser']['username']).to eq('newme')
-      end
-
-      it "A user can update their target and native language" do
-        query = <<~GQL
-          mutation {
-            updateUser(input: {
-              id: #{@existing_user.id}
-              targetId: "#{@target.id}"
-              nativeId: "#{@native.id}"
-            }) {
-              id
-              userLanguages{
+      describe 'happy paths' do
+        it "A user can be updated" do
+          query = <<~GQL
+            mutation {
+              updateUser(input: {
+                id: #{@existing_user.id}
+                email: "newme@email.com"
+                username: "newme"
+              }) {
                 id
-                userId
-                languageId
-                fluencyLevel
+                username
+                email
               }
             }
-          }
-        GQL
+          GQL
 
-        post '/graphql', params: { query: query }
+          post '/graphql', params: { query: query }
 
-        json = JSON.parse(response.body)
+          json = JSON.parse(response.body)
 
-        expect(json['data']['updateUser']['id']).to_not eq(nil)
-        expect(json['data']['updateUser']['userLanguages'][0]['id']).to_not eq(nil)
-        expect(json['data']['updateUser']['userLanguages'][0]['userId']).to eq(@existing_user.id.to_s)
-        expect(json['data']['updateUser']['userLanguages'][0]['languageId']).to eq(@target.id.to_s)
-        expect(json['data']['updateUser']['userLanguages'][0]['fluencyLevel']).to eq("target")
+          expect(json['data']['updateUser']['id']).to_not eq(nil)
+          expect(json['data']['updateUser']['email']).to eq('newme@email.com')
+          expect(json['data']['updateUser']['username']).to eq('newme')
+        end
 
-        expect(json['data']['updateUser']['userLanguages'][1]['id']).to_not eq(nil)
-        expect(json['data']['updateUser']['userLanguages'][1]['userId']).to eq(@existing_user.id.to_s)
-        expect(json['data']['updateUser']['userLanguages'][1]['languageId']).to eq(@native.id.to_s)
-        expect(json['data']['updateUser']['userLanguages'][1]['fluencyLevel']).to eq("native")
+        it "A user can update their target and native language" do
+          query = <<~GQL
+            mutation {
+              updateUser(input: {
+                id: #{@existing_user.id}
+                targetId: "#{@target.id}"
+                nativeId: "#{@native.id}"
+              }) {
+                id
+                userLanguages{
+                  id
+                  userId
+                  languageId
+                  fluencyLevel
+                }
+              }
+            }
+          GQL
+
+          post '/graphql', params: { query: query }
+
+          json = JSON.parse(response.body)
+
+          expect(json['data']['updateUser']['id']).to_not eq(nil)
+          expect(json['data']['updateUser']['userLanguages'][0]['id']).to_not eq(nil)
+          expect(json['data']['updateUser']['userLanguages'][0]['userId']).to eq(@existing_user.id.to_s)
+          expect(json['data']['updateUser']['userLanguages'][0]['languageId']).to eq(@target.id.to_s)
+          expect(json['data']['updateUser']['userLanguages'][0]['fluencyLevel']).to eq("target")
+
+          expect(json['data']['updateUser']['userLanguages'][1]['id']).to_not eq(nil)
+          expect(json['data']['updateUser']['userLanguages'][1]['userId']).to eq(@existing_user.id.to_s)
+          expect(json['data']['updateUser']['userLanguages'][1]['languageId']).to eq(@native.id.to_s)
+          expect(json['data']['updateUser']['userLanguages'][1]['fluencyLevel']).to eq("native")
+        end
+      end
+
+      describe 'sad paths' do
+        it 'A user cant update their native or target languages if they have none created' do
+          query = <<~GQL
+            mutation {
+              updateUser(input: {
+                id: #{@user_with_no_languages.id}
+                targetId: "#{@target.id}"
+                nativeId: "#{@native.id}"
+              }) {
+                id
+                userLanguages{
+                  id
+                  userId
+                  languageId
+                  fluencyLevel
+                }
+              }
+            }
+          GQL
+
+          post '/graphql', params: { query: query }
+
+          json = JSON.parse(response.body)
+
+          expect(json['data']['updateUser']['userLanguages']).to eq([])
+        end
       end
     end
   end
