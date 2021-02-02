1c75160c0580ca08d773f7dd90afc602e8deab8a
Logan Riffell <lkriffell@gmail.com>
2021-02-02 15:01:46 -0700

    Add testing for creating an availability

--- spec/mutations/availability/create_availability_spec.rb
+++ spec/mutations/availability/create_availability_spec.rb
@@ -7,6 +7,10 @@ module Mutations
         @user = User.create(email: 'JB@email.com', username: 'Jim Bobby', password: '1234')
         @start_dt = DateTime.new(2021, 1, 1, 9, 30).to_i
         @end_dt = DateTime.new(2021, 1, 1, 12, 30).to_i
+        @language1 = create(:language)
+        @language2 = create(:language)
+        create(:user_language, :native, language: @language1, user: @user)
+        create(:user_language, :target, language: @language2, user: @user)
       end
 
       def query(status:)
@@ -44,34 +48,74 @@ module Mutations
           }
         }" }
 
-        json = JSON.parse(response.body)
-        expect(json['data']['createAvailability']['id']).to_not be(nil)
-        expect(json['data']['createAvailability']['userId']).to eq(@user.id.to_s)
-        expect(json['data']['createAvailability']['startDateTime']).to eq('1609489800')
-        expect(json['data']['createAvailability']['endDateTime']).to eq('1609504200')
-        expect(json['data']['createAvailability']['status']).to eq('open')
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(json[:data][:createAvailability][:id]).to_not be(nil)
+        expect(json[:data][:createAvailability][:userId]).to eq(@user.id.to_s)
+        expect(json[:data][:createAvailability][:startDateTime]).to eq('1609489800')
+        expect(json[:data][:createAvailability][:endDateTime]).to eq('1609504200')
+        expect(json[:data][:createAvailability][:status]).to eq('open')
       end
 
       it 'An availability can be created as fulfilled' do
         post '/graphql', params: { query: query(status: 1) }
-        json = JSON.parse(response.body)
+        json = JSON.parse(response.body, symbolize_names: true)
 
-        expect(json['data']['createAvailability']['id']).to_not be(nil)
-        expect(json['data']['createAvailability']['userId']).to eq(@user.id.to_s)
-        expect(json['data']['createAvailability']['startDateTime']).to eq(@start_dt.to_s)
-        expect(json['data']['createAvailability']['endDateTime']).to eq(@end_dt.to_s)
-        expect(json['data']['createAvailability']['status']).to eq('fulfilled')
+        expect(json[:data][:createAvailability][:id]).to_not be(nil)
+        expect(json[:data][:createAvailability][:userId]).to eq(@user.id.to_s)
+        expect(json[:data][:createAvailability][:startDateTime]).to eq(@start_dt.to_s)
+        expect(json[:data][:createAvailability][:endDateTime]).to eq(@end_dt.to_s)
+        expect(json[:data][:createAvailability][:status]).to eq('fulfilled')
       end
 
       it 'An availability can be created as cancelled' do
         post '/graphql', params: { query: query(status: 2) }
-        json = JSON.parse(response.body)
+        json = JSON.parse(response.body, symbolize_names: true)
 
-        expect(json['data']['createAvailability']['id']).to_not be(nil)
-        expect(json['data']['createAvailability']['userId']).to eq(@user.id.to_s)
-        expect(json['data']['createAvailability']['startDateTime']).to eq(@start_dt.to_s)
-        expect(json['data']['createAvailability']['endDateTime']).to eq(@end_dt.to_s)
-        expect(json['data']['createAvailability']['status']).to eq('cancelled')
+        expect(json[:data][:createAvailability][:id]).to_not be(nil)
+        expect(json[:data][:createAvailability][:userId]).to eq(@user.id.to_s)
+        expect(json[:data][:createAvailability][:startDateTime]).to eq(@start_dt.to_s)
+        expect(json[:data][:createAvailability][:endDateTime]).to eq(@end_dt.to_s)
+        expect(json[:data][:createAvailability][:status]).to eq('cancelled')
+      end
+
+      it 'when an availability is created it tries to pair the user' do
+        japanese_learning_english = create(:user, id: 1)
+        start_dt = DateTime.new(2021, 1, 1, 14, 30).to_i
+        end_dt = DateTime.new(2021, 1, 1, 15, 30).to_i
+        english_learning_japanese = create(:user, id: 2)
+        english_learning_japanese_two = create(:user, id: 3)
+        create(:user_language, :native, language: @language1, user: japanese_learning_english)
+        create(:user_language, :target, language: @language2, user: japanese_learning_english)
+        create(:user_language, :native, language: @language2, user: english_learning_japanese)
+        create(:user_language, :target, language: @language1, user: english_learning_japanese)
+        create(:user_language, :native, language: @language2, user: english_learning_japanese_two)
+        create(:user_language, :target, language: @language1, user: english_learning_japanese_two)
+        english_learning_japanese.availabilities.create(start_date_time: DateTime.new(2021, 1, 1, 13, 30), end_date_time: DateTime.new(2021, 1, 1, 15, 45), status: 0)
+        english_learning_japanese_two.availabilities.create(start_date_time: DateTime.new(2021, 1, 1, 13, 30), end_date_time: DateTime.new(2021, 1, 1, 15, 45), status: 0)
+
+        post '/graphql', params: { query:
+          "mutation {
+          createAvailability(input: { params: {
+            userId: #{japanese_learning_english.id}
+            startDateTime: #{start_dt}
+            endDateTime: #{end_dt}
+          }}) {
+            id
+            userId
+            startDateTime
+            endDateTime
+            status
+          }
+        }" }
+
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(json[:data][:createAvailability][:id]).to_not be(nil)
+        expect(json[:data][:createAvailability][:userId]).to eq(japanese_learning_english.id.to_s)
+        expect(json[:data][:createAvailability][:startDateTime]).to eq(start_dt.to_s)
+        expect(json[:data][:createAvailability][:endDateTime]).to eq(end_dt.to_s)
+        expect(json[:data][:createAvailability][:status]).to eq('open')
+        
+        expect(Pairing.all.size).to eq(1)
       end
     end
   end
