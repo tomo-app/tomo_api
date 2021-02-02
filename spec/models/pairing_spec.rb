866202a7f58716a05a7cb24ec8d3f727d9a0c916
Logan Riffell <lkriffell@gmail.com>
2021-02-02 14:58:23 -0700

    Update pairing to not validate has_cancelled?

--- spec/models/pairing_spec.rb
+++ spec/models/pairing_spec.rb
@@ -8,7 +8,5 @@ RSpec.describe Pairing, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :date_time }
-    it { should validate_presence_of :user1_cancelled? }
-    it { should validate_presence_of :user2_cancelled? }
   end
 end
