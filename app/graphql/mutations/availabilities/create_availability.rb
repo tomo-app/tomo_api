2ae36d3b45d6dd297582259095ec8ebaf416bf4d
Logan Riffell <lkriffell@gmail.com>
2021-02-02 15:15:58 -0700

    Add minor refactor

--- app/graphql/mutations/availabilities/create_availability.rb
+++ app/graphql/mutations/availabilities/create_availability.rb
@@ -9,10 +9,8 @@ module Mutations
         availability = Availability.create(availability_params)
         open_slot = availability.find_availabilities_to_pair
         pairing = Pairing.create_pairing(availability, open_slot) unless open_slot.nil?
-        if pairing
-          availability.update(status: 'fulfilled')
-          open_slot[0].update(status: 'fulfilled')
-        end
+        availability.update(status: 'fulfilled') unless pairing.nil?
+        open_slot[0].update(status: 'fulfilled') unless pairing.nil?
         availability
       end
     end
