22c994bf6b1fa194f546af8a64381211f746dd6d
Logan Riffell <lkriffell@gmail.com>
2021-02-02 15:01:26 -0700

    Add testing for blocked ids user model method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of :username }
     it { should validate_presence_of :password }
   end
+
+  describe 'model methods' do
+    it 'blocked_ids' do
+      blocker = create(:user)
+      blocked_one = create(:user)
+      blocked_two = create(:user)
+      blocked_three = create(:user)
+      BlockedPairing.create(blocking_user: blocker, blocked_user: blocked_one)
+      BlockedPairing.create(blocking_user: blocker, blocked_user: blocked_two)
+      BlockedPairing.create(blocking_user: blocker, blocked_user: blocked_three)
+      expect(blocker.blocked_ids.size).to eq(3)
+    end
+  end
 end
