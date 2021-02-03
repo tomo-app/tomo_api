eb1d8a5c9914ca4ee2d8e1c78d0986f1f47d94b7
Leah Riffell <leah.riffell@gmail.com>
2021-02-02 21:00:11 -0700

    Symbolize names in json response tests

--- spec/mutations/users/create_user_spec.rb
+++ spec/mutations/users/create_user_spec.rb
@@ -4,17 +4,18 @@ module Mutations
   module Users
     RSpec.describe CreateUser, type: :request do
       it 'A user can be created' do
-        post '/graphql', params: { query: query(email: "user@tomo.com", username: "mari", password: "123", password_confirmation: "123") }
+        post '/graphql', params: { query: query(email: 'user@tomo.com', username: 'mari', password: '123', password_confirmation: '123') }
 
-        json = JSON.parse(response.body)
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        user = parsed[:data][:createUser]
 
-        expect(json['data']['createUser']['id']).to_not eq(nil)
+        expect(user[:id]).to_not eq(nil)
 
-        expect(json['data']['createUser']['username']).to be_a(String)
-        expect(json['data']['createUser']['username']).to eq('mari')
+        expect(user[:username]).to be_a(String)
+        expect(user[:username]).to eq('mari')
 
-        expect(json['data']['createUser']['email']).to be_a(String)
-        expect(json['data']['createUser']['email']).to eq('user@tomo.com')
+        expect(user[:email]).to be_a(String)
+        expect(user[:email]).to eq('user@tomo.com')
       end
 
       def query(email:, username:, password:, password_confirmation:)
