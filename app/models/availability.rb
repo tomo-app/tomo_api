fe3940020bb9011c5f4d5c8148e44e426ba8cdaf
Logan Riffell <lkriffell@gmail.com>
2021-02-03 11:15:58 -0700

    Fix rubocop offenses

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -8,16 +8,7 @@ class Availability < ApplicationRecord
   enum status: { 'open': 0, 'fulfilled': 1, 'cancelled': 2 }
 
   def find_availabilities_to_pair
-    user = User.find(user_id)
-    target = user.user_languages.find_by(fluency_level: 'target')
-    native = user.user_languages.find_by(fluency_level: 'native')
-
-    target_users = UserLanguage.where.not(user_id: user_id).where(fluency_level: 'native',
-                                                                  language_id: target.language_id).select(:user_id)
-    people_to_pair = UserLanguage.where.not(user_id: user_id).where(fluency_level: 'target',
-                                                                    language_id: native.language_id).where(user_id: target_users).order(:created_at)
-
-    if people_to_pair != []
+    unless people_to_pair.empty?
       Availability.where(start_date_time: start_date_time..end_date_time)
                   .or(Availability.where(end_date_time: start_date_time..end_date_time))
                   .or(Availability.where('availabilities.start_date_time < ?', start_date_time).where(
@@ -25,8 +16,22 @@ class Availability < ApplicationRecord
                       ))
                   .where.not(user_id: user_id)
                   .where.not(user_id: user.blocked_ids)
-                  .where(status: 'open')
-                  .where(user_id: people_to_pair.first.user_id)
+                  .where(status: 'open').where(user_id: people_to_pair.first.user_id)
     end
+  end
+
+  def people_to_pair
+    target = user.user_languages.find_by(fluency_level: 'target')
+    native = user.user_languages.find_by(fluency_level: 'native')
+
+    target_users = UserLanguage.where.not(user_id: user_id).where(fluency_level: 'native',
+                                                                  language_id: target.language_id).select(:user_id)
+    UserLanguage.where.not(user_id: user_id)
+                .where(fluency_level: 'target', language_id: native.language_id)
+                .where(user_id: target_users).order(:created_at)
+  end
+
+  def self.user
+    User.find(user_id)
   end
 end
