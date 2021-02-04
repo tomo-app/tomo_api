5d86696adebf413732461f4553f0d8ced6e23a2f
Logan Riffell <lkriffell@gmail.com>
2021-02-04 13:24:42 -0700

    Refactor create availability for cleaner code

--- app/graphql/mutations/availabilities/create_availability.rb
+++ app/graphql/mutations/availabilities/create_availability.rb
@@ -7,11 +7,15 @@ module Mutations
       def resolve(params:)
         availability_params = Hash params
         availability = Availability.create(availability_params)
-        open_slot = availability.find_availabilities_to_pair if availability
+        schedule_pairing(availability) if availability
+        availability
+      end
+
+      def schedule_pairing(availability)
+        open_slot = availability.find_availabilities_to_pair
         pairing = Pairing.create_pairing(availability, open_slot) unless open_slot.nil?
         availability.update(status: 'fulfilled') unless pairing.nil?
         open_slot[0].update(status: 'fulfilled') unless pairing.nil?
-        availability
       end
     end
   end
