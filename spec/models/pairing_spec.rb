377a818f7b5a89a9ae47680f1530da7a60becce3
Logan Riffell <lkriffell@gmail.com>
2021-02-04 12:53:20 -0700

    Add model tests for pairing

--- spec/models/pairing_spec.rb
+++ spec/models/pairing_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Pairing, type: :model do
   describe 'validations' do
     it { should validate_presence_of :date_time }
   end
+
+  describe 'model methods' do
+    before :each do
+      japanese_learning_english = create(:user)
+      english_learning_japanese = create(:user)
+      @availability = japanese_learning_english.availabilities.create(start_date_time: DateTime.new(2021, 1, 1, 14, 30), end_date_time: DateTime.new(2021, 1, 1, 15, 30), status: 0)
+      @open_slot = english_learning_japanese.availabilities.create(start_date_time: DateTime.new(2021, 1, 1, 13, 30), end_date_time: DateTime.new(2021, 1, 1, 15, 30), status: 0)
+    end
+    it 'create_pairing' do
+      Pairing.create_pairing(@availability, [@open_slot])
+
+      expect(Pairing.all.size).to eq(1)
+    end
+
+    it 'determine_time_of_pairing' do
+      time = Pairing.determine_time_of_pairing(@availability, @open_slot)
+
+      expect(time).to eq(@availability.start_date_time)
+    end
+  end
 end
