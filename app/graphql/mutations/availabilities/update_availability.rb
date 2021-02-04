d7bb944f50bc9cc813060c6db4f983387a2158cc
Logan Riffell <lkriffell@gmail.com>
2021-02-04 13:23:59 -0700

    Add function to schedule pairing on updateAvailability

--- app/graphql/mutations/availabilities/update_availability.rb
+++ app/graphql/mutations/availabilities/update_availability.rb
@@ -12,7 +12,17 @@ module Mutations
         availability = Availability.find(arguments[:id])
         arguments.delete(:id)
         availability.update(arguments)
+        schedule_pairing(availability) if availability
         availability
+      end
+
+      def schedule_pairing(availability)
+        if availability.status != 'cancelled' && availability.status != 'fulfilled'
+          open_slot = availability.find_availabilities_to_pair
+          pairing = Pairing.create_pairing(availability, open_slot) unless open_slot.empty?
+          availability.update(status: 'fulfilled') unless pairing.nil?
+          open_slot[0].update(status: 'fulfilled') unless pairing.nil?
+        end
       end
     end
   end
