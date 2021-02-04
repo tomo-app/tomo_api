7343930b0f75b2ed7b2e7b82e22e1e99d771ee84
Leah Riffell <leah.riffell@gmail.com>
2021-02-04 14:04:38 -0700

    Create query for get all user's pairings and spec

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
+    # ----- languages -----
     field :get_languages, [Types::LanguageType], null: false, description: 'Reurns a list of all languages'
 
     def get_languages
       Language.all
     end
     
+    # ----- availabilities -----
     field :get_availabilities, [Types::AvailabilityType], null: false, description: 'Availabilities (sorted newest to oldest) by user_id' do
       argument :user_id, ID, required: true
       argument :status, String, required: false
@@ -21,12 +23,22 @@ module Types
       end
     end
 
+    # ----- users -----
     field :get_user, Types::UserType, null: false, description: 'Returns a single user by id' do
       argument :id, ID, required: true
     end
     
     def get_user(id:)
       User.find(id)
+    end
+
+    # ----- pairings -----
+    field :get_pairings, [Types::PairingType], null: false, description: 'Returns all pairings for a user by id' do
+      argument :user_id, ID, required: true
+    end
+
+    def get_pairings(user_id:)
+      Pairing.where(user1_id: user_id).or(Pairing.where(user2_id: user_id))
     end
   end
 end
