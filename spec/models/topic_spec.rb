32a72a97d526214db489b9e7421622bc2933b72a
Leah Riffell <leah.riffell@gmail.com>
2021-02-04 20:35:20 -0700

    Add topic model test for translations_for_languages

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -17,5 +17,25 @@ RSpec.describe Topic, type: :model do
         expect(Topic.random).to be_a(Topic)
       end
     end
+
+    describe 'translations for languages' do
+      it 'gets topic translations for specific languages' do
+        topic = create(:topic)
+        language_1 = create(:language)
+        language_2 = create(:language)
+        language_3 = create(:language)
+        translation_1 = create(:topic_translation, topic: topic, language: language_1)
+        translation_2 = create(:topic_translation, topic: topic, language: language_2)
+        translation_2 = create(:topic_translation, topic: topic, language: language_3)
+
+        translations = topic.translations_for_languages([language_1.id, language_2.id])
+        lang_ids = translations.map {|translation| translation.language_id}
+
+        expect(translations.size).to eq(2)
+        expect(lang_ids.include?(language_1.id)).to eq(true)
+        expect(lang_ids.include?(language_2.id)).to eq(true)
+        expect(lang_ids.include?(language_3.id)).to eq(false)
+      end
+    end
   end
 end
