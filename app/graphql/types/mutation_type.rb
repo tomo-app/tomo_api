9d74ea1f2cc444fc7e955ce6f22a9c78a575a126
Leah Riffell <leah.riffell@gmail.com>
2021-02-05 19:44:52 -0700

    Add cancel pairing mutation and test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,5 +6,6 @@ module Types
     field :update_availability, mutation: Mutations::Availabilities::UpdateAvailability
     field :create_user_language, mutation: Mutations::UserLanguages::CreateUserLanguage
     field :create_blocked_pairing, mutation: Mutations::BlockedPairings::CreateBlockedPairing
+    field :cancel_pairing, mutation: Mutations::Pairings::CancelPairing
   end
 end
