b3b117a0d6b6e3b1ec9e8f42f7bf76e90063ae65
Leah Riffell <leah.riffell@gmail.com>
2021-02-06 13:09:22 -0700

    Add http prefix to cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'tomo-app.herokuapp.com'
-
+    origins 'http://localhost:3000', 'https://tomo-app.herokuapp.com'
+    
     resource '',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
