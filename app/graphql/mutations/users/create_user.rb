63f85b84c6a7c279e3937c07ecaaf043f5bde718
Logan Riffell <lkriffell@gmail.com>
2021-02-07 14:57:11 -0700

    Refactor create_user to DRY up code and add error message

--- app/graphql/mutations/users/create_user.rb
+++ app/graphql/mutations/users/create_user.rb
@@ -6,10 +6,14 @@ module Mutations
 
       def resolve(params:)
         user_params = Hash params
-        if user_params[:password] == user_params[:password_confirmation]
+        passwords_match = user_params[:password] == user_params[:password_confirmation]
+        username_taken = User.exists?(username: user_params[:username])
+        if passwords_match && !username_taken
           User.create(user_params)
-        else
+        elsif !passwords_match
           GraphQL::ExecutionError.new('password and confirmation must match')
+        elsif username_taken
+          GraphQL::ExecutionError.new('That username aleady exists')
         end
       end
     end
