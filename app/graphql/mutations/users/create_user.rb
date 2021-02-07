aaad8890cda4ac3f6c6b444f1075e9b8a885466d
Logan Riffell <lkriffell@gmail.com>
2021-02-07 15:03:44 -0700

    Check that email isnt taken when user is created

--- app/graphql/mutations/users/create_user.rb
+++ app/graphql/mutations/users/create_user.rb
@@ -8,12 +8,15 @@ module Mutations
         user_params = Hash params
         passwords_match = user_params[:password] == user_params[:password_confirmation]
         username_taken = User.exists?(username: user_params[:username])
-        if passwords_match && !username_taken
+        email_taken = User.exists?(email: user_params[:email])
+        if passwords_match && !username_taken && !email_taken
           User.create(user_params)
         elsif !passwords_match
           GraphQL::ExecutionError.new('password and confirmation must match')
         elsif username_taken
           GraphQL::ExecutionError.new('That username aleady exists')
+        elsif email_taken
+          GraphQL::ExecutionError.new('That email is already taken')
         end
       end
     end
