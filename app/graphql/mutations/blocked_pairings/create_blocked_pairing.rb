1c5184fd10e3493a593dc2b903eb274214c4ee5f
Leah Riffell <leah.riffell@gmail.com>
2021-02-15 15:58:21 -0700

    Add error message for duplicate blocked pairing creation

--- app/graphql/mutations/blocked_pairings/create_blocked_pairing.rb
+++ app/graphql/mutations/blocked_pairings/create_blocked_pairing.rb
@@ -7,10 +7,18 @@ module Mutations
 
       def resolve(params:)
         blocked_pairing_params = Hash params
+        return handle_already_exists_error if BlockedPairing.blocked_pairing_exists?(
+          blocked_pairing_params[:blocking_user_id], blocked_pairing_params[:blocked_user_id]
+        )
+
         BlockedPairing.create(
           blocking_user_id: blocked_pairing_params[:blocking_user_id],
           blocked_user_id: blocked_pairing_params[:blocked_user_id]
         )
+      end
+
+      def handle_already_exists_error
+        GraphQL::ExecutionError.new('this user has already been blocked')
       end
     end
   end
